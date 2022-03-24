a2a4bdeddbd0c20dee79e2e63b8d79d8d6dc6eb6
Yasar Altamirano <droidhp.ya@gmail.com>
2022-03-24 16:50:29 -0600

    Add useEffect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
-  <CounterWithCustomHook />,
+  <FormWithCustomHook />,
   document.getElementById('root')
 );
