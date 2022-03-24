6d81a92383d3d3523b48da7498ec6a9dee896fcf
Yasar Altamirano <droidhp.ya@gmail.com>
2022-03-24 00:39:14 -0600

    Add useState and useCounter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HooksApp } from './HooksApp';
+import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { HooksApp } from './HooksApp';
 
 ReactDOM.render(
-  <HooksApp />,
+  <CounterWithCustomHook />,
   document.getElementById('root')
 );
